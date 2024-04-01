53f74500b2f21fdd02cb11004ed92dccdfd4e782
kiweeherman <137016035+kiweeherman@users.noreply.github.com>
2024-04-01 00:23:48 -0500

    Minor Appearance Fixes

--- src/index.js
+++ src/index.js
@@ -8,6 +8,9 @@ function displaySheet(response) {
 }
 function generateSheet(event) {
   event.preventDefault();
+  let sheet = document.getElementById("character-sheet");
+  sheet.classList.add("white");
+  sheet.innerHTML = "Processing...";
   let userInstructions = document.getElementById("user-instructions");
   let context =
     "You are a straight-to-the point dungeon master for dungeones and dragons. You love helping people make their own unique characters with complex backstories. The character sheets you generate are quick to the point lists with the following categories in basic HTML. Name, Race, Class, Level, Hit Points, Speed, Initiative, Armor Class, and Backstory. Please do not add or remove categories or break from this template. Speed should be represented soley by a number with no measurement following. Lastly, do your best to incorportate the user hint at the end of the prompt.";
